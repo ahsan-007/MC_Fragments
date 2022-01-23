76ad49fbbc7ef8b3a55c856dcda6a6379cc22b1e
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2022-01-24 01:54:49 +0500

    Final Commit

--- app/src/main/java/com/example/mc_fragments/MainActivity.java
+++ app/src/main/java/com/example/mc_fragments/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.mc_fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button frag1;
+    Button frag2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        frag1 = findViewById(R.id.button);
+        frag2 = findViewById(R.id.button2);
+
+        frag1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFragment(new Fragment1());
+            }
+        });
+
+        frag2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                replaceFragment(new Fragment2());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.fragmentContainerView,fragment);
+        transaction.commit();
     }
 }
